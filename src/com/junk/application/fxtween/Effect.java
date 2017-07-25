616d75d1b8b7be0880379e6f71341a6ec772ead3
elmerhd <>
2017-07-25 12:56:08 -0700

    created test

--- src/com/junk/application/fxtween/Effect.java
+++ src/com/junk/application/fxtween/Effect.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.junk.application.fxtween;
 
 import aurelienribon.tweenengine.TweenAccessor;
